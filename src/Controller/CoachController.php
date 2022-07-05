e1fa732d176c678ff7b239d8b44102150f0ab955
CindySanchez06 <cindyk_98@hotmail.com>
2022-07-04 23:17:32 -0500

    almacenando entrenador en db

--- src/Controller/CoachController.php
+++ src/Controller/CoachController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Coach;
+use App\Form\CoachType;
 use App\Services\CoachService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -24,6 +27,26 @@ class CoachController extends AbstractController
         $coachs = $this->coachService->getAllCoachs();
         return $this->render('coach/index.html.twig',[
             'coachs' => $coachs
+        ]);
+    }
+
+    /**
+     * @Route("/new-coach", name="new_coach")
+     */
+    public function create(Request $request)
+    {
+        $coach = new Coach();
+        $form = $this->createForm(CoachType::class, $coach);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $coach = $form->getData();
+            $this->coachService->addCoach($coach);
+            return $this->redirectToRoute('coach');
+        }
+
+        return $this->renderForm('coach/create.html.twig',[
+            'form' => $form
         ]);
     }
 }
--- src/Services/CoachService.php
+++ src/Services/CoachService.php
@@ -3,14 +3,17 @@
 namespace App\Services;
 
 use App\Repository\CoachRepository;
+use Doctrine\ORM\EntityManagerInterface;
 
 class CoachService
 {
     private $coachRepository;
+    private $entityManager;
 
-    public function __construct(CoachRepository $coachRepository)
+    public function __construct(CoachRepository $coachRepository,  EntityManagerInterface $entityManager )
     {
         $this->coachRepository = $coachRepository;
+        $this->entityManager = $entityManager;
     }
 
     public function getAllCoachs()
@@ -18,4 +21,11 @@ class CoachService
         return $this->coachRepository->findAll();
     }
 
+    public function addCoach($data)
+    {
+        //$name = $data->name;
+        //$avatar = $data->avatar;
+        $this->entityManager->persist($data);
+        $this->entityManager->flush();
+    }
 }
